50a8f454cd6329ac3c8d93763eb1e9d0cb3b7c2d
andreas riedmann <andreas@riedmann.rocks>
2024-09-13 11:42:10 +0200

    init

---
+++ src/ForLoop.java
@@ -0,0 +1,5 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("for looe");
+    }
+}
---
+++ src/IfStatement.java
@@ -0,0 +1,28 @@
+public class IfStatement {
+    public static void main(String[] args) {
+
+        byte b = 4;
+        short s = 8;
+        int i = 7;
+        long l = 10;
+
+        // kommazahl
+        float f = 4.4f;
+        double d = 4.4;
+
+        // Character
+        char c = 'a';
+
+        boolean isValid = true;
+
+        String text = new String("Super Sache");
+        String[] splitted = text.split("");
+        System.out.println(text);
+        System.out.println(splitted);
+
+
+
+
+
+    }
+}
