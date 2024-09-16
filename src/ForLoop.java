fb0eaa0c4142f3bcb9e512be9a3c8a4470958700
andreas riedmann <andreas@riedmann.rocks>
2024-09-16 08:40:59 +0200

    ForLoop Done

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -2,5 +2,14 @@ public class ForLoop {
     public static void main(String[] args) {
         System.out.println("for looe");
         System.out.println("yes");
+        
+        int i = -7;
+        double d = -7.4;
+
+        for (int j = 10; j >= 4; j--) {
+            System.out.println("j:" + j);
+        }
+
+        //10
     }
 }
